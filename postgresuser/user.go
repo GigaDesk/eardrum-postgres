ecb453cf499bcbf255f000a525578e4763b8b9b6
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-06-16 19:19:28 +0300

    feat: add PhoneNumberExists data type

--- postgresuser/user.go
+++ postgresuser/user.go
@@ -16,46 +16,106 @@ type User struct {
 }
 
 // Returns the unique ID of the user
-func (s User) GetID() int64 {
-	return int64(s.ID)
+func (u User) GetID() int64 {
+	return int64(u.ID)
 }
 
 // Returns the creation timestamp of the user
-func (s User) GetCreatedAt() time.Time {
-	return s.CreatedAt
+func (u User) GetCreatedAt() time.Time {
+	return u.CreatedAt
 }
 
 // Returns the update timestamp of the user
-func (s User) GetUpdatedAt() time.Time {
-	return s.UpdatedAt
+func (u User) GetUpdatedAt() time.Time {
+	return u.UpdatedAt
 }
 
 // Returns the deletion timestamp of the user
-func (s User) GetDeletedAt() time.Time {
-	return s.DeletedAt.Time
+func (u User) GetDeletedAt() time.Time {
+	return u.DeletedAt.Time
 }
 
 // Returns the name of the user
-func (s User) GetName() string {
-	return s.Name
+func (u User) GetName() string {
+	return u.Name
 }
 
 // Returns the phone number of the user
-func (s User) GetPhoneNumber() string {
-	return s.PhoneNumber
+func (u User) GetPhoneNumber() string {
+	return u.PhoneNumber
 }
 
 // Returns the account balance in cents of the user
-func (s User) GetAccountBalanceInCents() int64 {
-	return s.AccountBalanceInCents
+func (u User) GetAccountBalanceInCents() int64 {
+	return u.AccountBalanceInCents
 }
 
 // Returns the security password of the user
-func (s User) GetPassword() string {
-	return s.Password
+func (u User) GetPassword() string {
+	return u.Password
 }
 
 // Returns the security PIN code of the user
-func (s User) GetPinCode() string {
-	return s.PinCode
+func (u User) GetPinCode() string {
+	return u.PinCode
+}
+
+
+type UnverifiedUser struct {
+	gorm.Model
+	Name                  string    // Name of the unverified user
+	PhoneNumber           string    // Phone number of the unverified user
+	Password              string    // Security password of the unverified user
+	AccountBalanceInCents int64     // The unverified user's account balance in cents
+	PinCode               string    // The security PIN code of the unverified user
+}
+
+// Returns the unique ID of the unverified user
+func (u UnverifiedUser) GetID() int64 {
+	return int64(u.ID)
+}
+
+// Returns the creation timestamp of the unverified user
+func (u UnverifiedUser) GetCreatedAt() time.Time {
+	return u.CreatedAt
+}
+
+// Returns the update timestamp of the unverified user
+func (u UnverifiedUser) GetUpdatedAt() time.Time {
+	return u.UpdatedAt
+}
+
+// Returns the deletion timestamp of the unverified user
+func (u UnverifiedUser) GetDeletedAt() time.Time {
+	return u.DeletedAt.Time
+}
+
+// Returns the name of the unverified user
+func (u UnverifiedUser) GetName() string {
+	return u.Name
+}
+
+// Returns the phone number of the unverified user
+func (u UnverifiedUser) GetPhoneNumber() string {
+	return u.PhoneNumber
+}
+
+// Returns the account balance in cents of the unverified user
+func (u UnverifiedUser) GetAccountBalanceInCents() int64 {
+	return u.AccountBalanceInCents
+}
+
+// Returns the security password of the unverified user
+func (u UnverifiedUser) GetPassword() string {
+	return u.Password
+}
+
+// Returns the security PIN code of the unverified user
+func (u UnverifiedUser) GetPinCode() string {
+	return u.PinCode
+}
+
+type PhoneNumberExists struct {
+	Verified bool
+	Unverified bool
 }
