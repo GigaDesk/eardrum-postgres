63c76afafc41003cd3bbb028665cb22b489f9a19
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-08-15 15:27:50 +0300

    refactor: add ShopID to category creation

--- postgresproduct/createcategory.go
+++ postgresproduct/createcategory.go
@@ -5,12 +5,13 @@ import (
 	"gorm.io/gorm"
 )
 
-//create a category record
-func CreateCategory(c product.NewCategory, Db *gorm.DB) (product.Category, error) {
+// create a category record
+func CreateCategory(c product.NewCategory, Db *gorm.DB, shop_id uint) (product.Category, error) {
 	//create category data
 	category := &Category{
 		Name:        c.GetName(),
 		Description: c.GetDescription(),
+		ShopID:      shop_id,
 	}
 
 	//create a category record in the database and return if operation succeeds
