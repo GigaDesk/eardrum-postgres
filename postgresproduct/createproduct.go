b448327d7fa09b0280d9da3fdac0e3a4ec068695
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-08-15 15:43:20 +0300

    refactor: add ShopID in product creation

--- postgresproduct/createproduct.go
+++ postgresproduct/createproduct.go
@@ -5,12 +5,13 @@ import (
 	"gorm.io/gorm"
 )
 
-//create a product record
-func CreateProduct(p product.NewProduct, Db *gorm.DB) (product.Product, error) {
+// create a product record
+func CreateProduct(p product.NewProduct, Db *gorm.DB, shop_id uint) (product.Product, error) {
 	//create product data
 	product := &Product{
-		Name:        p.GetName(),
-		PricePerUnitInCents: p.GetPricePerUnitInCents(),
+		Name:                p.GetName(),
+		PricePerUnitInCents: uint(p.GetPricePerUnitInCents()),
+		ShopID:              shop_id,
 	}
 
 	//create a shop record in the database and return if operation succeeds
