eb82d32ce89d7bf3400752deaa9385d813ef6932
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-09-20 06:33:39 +0300

    feat: Add functions to retrieve transactions for a user and a shop.

--- transaction/gettransaction.go
+++ transaction/gettransaction.go
@@ -5,7 +5,7 @@ import (
 	"gorm.io/gorm"
 )
 
-//Gets a transaction by its unique id
+// Gets a transaction by its unique id
 func GetTransactionWithId(Db *gorm.DB, Id int) (transaction.Transaction, error) {
 	var transaction *Transaction
 	//fetch the record from the database
@@ -16,7 +16,7 @@ func GetTransactionWithId(Db *gorm.DB, Id int) (transaction.Transaction, error) 
 	return transaction, nil
 }
 
-//Gets all the transactions registered in the database
+// Gets all the transactions registered in the database
 func GetTransactions(Db *gorm.DB) ([]transaction.Transaction, error) {
 
 	var transactions []*Transaction
@@ -32,4 +32,24 @@ func GetTransactions(Db *gorm.DB) ([]transaction.Transaction, error) {
 	}
 
 	return transactionslist, nil
+}
+
+// GetTransactionsForUser retrieves all transactions for a specific user.
+// It leverages the foreign key relationship to filter the results.
+func GetTransactionsForUser(db *gorm.DB, userID uint) ([]Transaction, error) {
+	var transactions []Transaction
+	// GORM automatically filters on the UserID foreign key.
+	if err := db.Where("user_id = ?", userID).Find(&transactions).Error; err != nil {
+		return nil, err
+	}
+	return transactions, nil
+}
+
+// GetTransactionsForShop retrieves all transactions for a specific shop.
+func GetTransactionsForShop(db *gorm.DB, shopID uint) ([]Transaction, error) {
+	var transactions []Transaction
+	if err := db.Where("shop_id = ?", shopID).Find(&transactions).Error; err != nil {
+		return nil, err
+	}
+	return transactions, nil
 }
